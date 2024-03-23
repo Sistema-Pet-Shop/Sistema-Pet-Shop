e6151f9a97775c42a734a0dc93bc25d5c5e486ce
PedroGustavo99 <pedrogustavojunior.acdc@gmail.com>
2024-03-23 12:26:03 -0300

    subindo excluir serviço back/front

---
+++ sistema_petshop2024/excluir_servico_backend.php
@@ -0,0 +1,11 @@
+<?php
+	error_reporting(0);
+	include "conexao.php";
+	$idservico=$_POST['idservico'];
+
+	$inserir = mysql_query("DELETE FROM servico
+							WHERE idservico ='$idservico'") 
+							or die (mysql_error());
+	mysql_close ($db);
+	include "consulta_servico.php";
+?>
---
+++ sistema_petshop2024/excluir_servico_frontend.php
@@ -0,0 +1,23 @@
+<!--Início Excluir-->
+<center>
+    <body bgcolor="lightsteelblue">
+    <form method="POST" action="excluir_servico_backend.php">
+        <p><br>
+            Codigo:<select size="1" name="idservico">
+            <?php
+                // Gera a lista de codigo ocupacao
+                include "conexao.php";
+                $res=mysql_query("select * from servico");
+                while ($registro=mysql_fetch_row($res))
+                {
+                $cod=$registro[0];
+                $nome=$registro[1];
+                echo "<option value=\"$cod\">$nome</option>\n";
+                }
+            ?>
+            </select><br><br>
+        </p> <br>
+        <input type="submit" name="Submit" value="Excluir">
+    </form>
+</center>
+<!--Fim Excluir-->
